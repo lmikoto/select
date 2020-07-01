1c31ac1564e89f25930bd5da5979168c65cdbdb5
lmikoto (from Travis CI) <lmikoto@outlook.com>
2020-07-01 03:40:38 +0000

    Deploy tools to github.com/lmikoto/select.git:gh-pages

---
+++ precache-manifest.483db03fd73620c23e2b6b4941234f01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b8a12fa29e33accfdffc1819b4debd9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9cedd0386a52cfde65a",
+    "url": "/static/css/2.c27a359e.chunk.css"
+  },
+  {
+    "revision": "6ff9c0d669aa0d329f91",
+    "url": "/static/css/main.fc6ae21c.chunk.css"
+  },
+  {
+    "revision": "e9cedd0386a52cfde65a",
+    "url": "/static/js/2.eb02f627.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.eb02f627.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ff9c0d669aa0d329f91",
+    "url": "/static/js/main.5e98abbd.chunk.js"
+  },
+  {
+    "revision": "de7ef5009af241ff6598",
+    "url": "/static/js/runtime-main.e4f1e7a8.js"
+  }
+]);
